f4b3f32487729d71bda468bc1e83bf085be74745
Valentina Vecchio <valentina.vecchio12@gmail.com>
2022-12-13 20:51:23 +0000

    moved to columnar analysis

--- applyBDT.py
+++ applyBDT.py
@@ -12,7 +12,7 @@ def main():
     bst.load_model("twoprong.json")
 
     variables = ["ljet_tau21","ljet_tau32","ljet_Split12","ljet_Split23",
-                 "ljet_flavourlabel","ljet_Split34","ljet_ECF1"]
+                 "ljet_Split34","ljet_ECF1"]
 
     apply_BDT_score(bst, variables)
 
@@ -23,38 +23,26 @@ def apply_BDT_score(bst, variables):
     #file_name = "364703.root"
     input_file = uproot.concatenate("../"+file_name+":nominal",variables,
                                     allow_missing=True)
-    bdt_score = []
-    with alive_bar(len(input_file)) as bar:
-        for count,ev in enumerate(input_file):
-            if count>1000: break
-            bar()
-            array_lenght = len(ev["ljet_tau21"])
-            lj_score = np.zeros(array_lenght,dtype=np.float64)
-            lj_index = 0
-            for t21,t32,s12,s23,s34,ecf1 in zip(ev["ljet_tau21"],
-                                                ev["ljet_tau32"],
-                                                ev["ljet_Split12"],
-                                                ev["ljet_Split23"],
-                                                ev["ljet_Split34"],
-                                                ev["ljet_ECF1"]):
-                data = {}
-                data["ljet_tau21_0"] = t21
-                data["ljet_tau32_0"] = t32
-                data["ljet_Split12_0"] = s12
-                data["ljet_Split23_0"] = s23
-                data["ljet_Split34_0"] = s34
-                data["ljet_ECF1_0"] = ecf1
-                lj_index+=1
-                data = ak.to_pandas(data)
-                if data.shape[1]!=6: continue
-                test_proba = bst.predict_proba(data)
-                jet_score = test_proba[:,0]
-                lj_score[lj_index-1] = jet_score
-            bdt_score.append(lj_score)
+
+    tau_21 = ak.Array(input_file["ljet_tau21"])
+
+    input_file = ak.to_pandas(input_file[variables])
+    input_file = rename_variables(input_file,variables)
+    test_proba = bst.predict_proba(input_file)
+    bdt_scores = ak.Array(test_proba[:,0])
+    counts = ak.num(tau_21)
+    unflattened_bdt_score = ak.unflatten(bdt_scores,counts)
+
     with uproot.recreate("tree_tester_"+file_name) as file:
-        file["nominal"] = {"bdt_score": ak.Array(bdt_score)}
+        file["nominal"] = {"bdt_score":unflattened_bdt_score}
 
     return 0
+
+#remap data to have same variables names as in training
+def rename_variables(input_file, bdt_vars):
+    for var in bdt_vars: input_file[var+"_0"] = input_file[var]
+    input_file = input_file.drop(columns=bdt_vars)
+    return input_file
 
 #main
 if __name__ == '__main__':
